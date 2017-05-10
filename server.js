0799595535e6693826374c3cf15702d4879e42f7
tqvarnst <tqvarnst@redhat.com>
2017-05-10 22:17:59 +0200

    First version

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //  OpenShift sample Node application
 var express = require('express'),
+    path    = require('path'),
     fs      = require('fs'),
     app     = express(),
     eps     = require('ejs'),
@@ -58,23 +59,23 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+// app.get('/', function (req, res) {
+//   // try to initialize the db on every request if it's not already
+//   // initialized.
+//   if (!db) {
+//     initDb(function(err){});
+//   }
+//   if (db) {
+//     var col = db.collection('counts');
+//     // Create a document with request IP and current time of request
+//     col.insert({ip: req.ip, date: Date.now()});
+//     col.count(function(err, count){
+//       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+//     });
+//   } else {
+//     res.render('index.html', { pageCountMessage : null});
+//   }
+// });
 
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
@@ -90,6 +91,8 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
+
+app.use(express.static(path.join(__dirname + '/public')));
 
 // error handling
 app.use(function(err, req, res, next){
